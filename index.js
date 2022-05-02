8a4f677eac551be0c1b283a1d464ff9cf8cc9092
Juan Martín Seery <me@juanm04.com>
2022-05-02 10:16:51 -0300

    Add inferring of `node` in `MapFunction` from `tree`
    Related-to GH-3.
    Closes GH-4.
    
    Reviewed-by: Titus Wormer <tituswormer@gmail.com>

--- index.js
+++ index.js
@@ -1,46 +1,29 @@
 /**
- * @typedef {import('unist').Parent} Parent
- * @typedef {import('unist').Position} Position
  * @typedef {import('unist').Node} Node
- * @typedef {Record<string, unknown> & {type: string, position?: Position|undefined}} NodeLike
- */
-
-/**
- * Function called with a node to produce a new node.
- *
- * @callback MapFunction
- * @param {NodeLike|Node} node Current node being processed
- * @param {number} [index] Index of `node`, or `null`
- * @param {Parent} [parent] Parent of `node`, or `null`
- * @returns {NodeLike|Node} Node to be used in the new tree. Its children are not used: if the original node has children, those are mapped.
  */
 
 /**
  * Unist utility to create a new tree by mapping all nodes with the given function.
  *
- * @param {NodeLike|Node} tree Tree to map
- * @param {MapFunction} iteratee Function that returns a new node
- * @returns {NodeLike|Node} New mapped tree.
+ * @template {Node} Tree
+ * @param {Tree} tree Tree to map
+ * @param {import('./complex-types').MapFunction<Tree>} iteratee Function that returns a new node
+ * @returns {Tree} New mapped tree.
  */
 export function map(tree, iteratee) {
+  // @ts-expect-error Looks like a children.
   return preorder(tree, null, null)
 
-  /**
-   * @param {NodeLike|Node} node
-   * @param {number} [index]
-   * @param {Parent} [parent]
-   * @returns {Node}
-   */
+  /** @type {import('./complex-types').MapFunction<Tree>} */
   function preorder(node, index, parent) {
     var newNode = Object.assign({}, iteratee(node, index, parent))
 
     if ('children' in node) {
       // @ts-expect-error Looks like a parent.
       newNode.children = node.children.map(function (
-        /** @type {Node} */ child,
+        /** @type {import('./complex-types').InclusiveDescendant<Tree>} */ child,
         /** @type {number} */ index
       ) {
-        // @ts-expect-error Looks like a parent.
         return preorder(child, index, node)
       })
     }
